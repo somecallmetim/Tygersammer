64aeb2ab1ceb1ddeeacc54efb09bc699b432b17e
Tim Bauer <timbauer@ymail.com>
2021-06-24 10:57:05 -0700

    first commit

---
+++ src/main/java/com/tygershammer/tygersammer/TygersammerApplication.java
@@ -0,0 +1,13 @@
+package com.tygershammer.tygersammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TygersammerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TygersammerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tygershammer/tygersammer/TygersammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tygershammer.tygersammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TygersammerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
