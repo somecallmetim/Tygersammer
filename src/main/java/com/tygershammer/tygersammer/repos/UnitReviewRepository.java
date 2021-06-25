aa8a74b7db0be3d3456f663a870a66852ee042a3
Tim Bauer <timbauer@ymail.com>
2021-06-25 13:34:25 -0700

    added UnitReviewRepository

---
+++ src/main/java/com/tygershammer/tygersammer/repos/UnitReviewRepository.java
@@ -0,0 +1,7 @@
+package com.tygershammer.tygersammer.repos;
+
+import com.tygershammer.tygersammer.models.UnitReview;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitReviewRepository extends CrudRepository<UnitReview, Long> {
+}
