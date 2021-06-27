e620dbc499fbf46963b85192419438cc0c5b8e29
Tim Bauer <timbauer@ymail.com>
2021-06-26 22:29:55 -0700

    you can now create new and view all reviews associated with a unit :)

--- src/main/java/com/tygershammer/tygersammer/controllers/UnitController.java
+++ src/main/java/com/tygershammer/tygersammer/controllers/UnitController.java
@@ -1,9 +1,12 @@
 package com.tygershammer.tygersammer.controllers;
 
 import com.tygershammer.tygersammer.models.Unit;
+import com.tygershammer.tygersammer.models.UnitReview;
 import com.tygershammer.tygersammer.repos.UnitRepoInterface;
+import com.tygershammer.tygersammer.repos.UnitReviewRepoInterface;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/unit")
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class UnitController {
 
     private UnitRepoInterface unitRepoInterface;
+    private UnitReviewRepoInterface unitReviewRepoInterface;
 
-    public UnitController(UnitRepoInterface unitRepoInterface){
+    public UnitController(UnitRepoInterface unitRepoInterface, UnitReviewRepoInterface unitReviewRepoInterface){
         this.unitRepoInterface = unitRepoInterface;
+        this.unitReviewRepoInterface = unitReviewRepoInterface;
     }
 
     @GetMapping("/new")
@@ -40,6 +45,15 @@ public class UnitController {
     public String getUnitByName(Model model, @PathVariable String name){
         Unit unit = unitRepoInterface.findByName(name);
         model.addAttribute(unit);
+        return "getUnitByName";
+    }
+
+    @PostMapping("/{name}/addReview")
+    public String addReview(@PathVariable String name, @RequestParam String review, ModelMap modelMap){
+        Unit unit = unitRepoInterface.findByName(name);
+        UnitReview unitReview = new UnitReview(review, unit);
+        unitReviewRepoInterface.save(unitReview);
+        modelMap.addAttribute(unit);
         return "getUnitByName";
     }
 }
